636dacc2668f8f7ca045c572c5b875d0891d0d40
machopman <machopman.shop@gmail.com>
2018-05-07 18:23:40 +0700

    Update director.py

--- director.py
+++ director.py
@@ -28,7 +28,7 @@ def movie_director(event,question,userid):
                     return 'ยังไม่มีข้อมูลผู้กำกับหนังเรื่องนี้เลยครับ'
         if found == False:
             return 'ยังไม่มีข้อมูลผู้กำกับหนังเรื่องนี้เลยครับ'
-    elif (movie_name=='')and (searchMovieNameInDic(question)==''):
+    elif (movie_name=='')and (searchMovieNameInDic(question)[0]==''):
         mov = findmovie(userid)
         movie_name = mov.lower().replace(' ','')
         URL = "http://mandm.plearnjai.com/API/id_nameMovie.php?key=mandm"
