df1ab8bc7c944f8747d6b7aced50ed346bb03584
machopman <machopman.shop@gmail.com>
2018-05-07 18:22:35 +0700

    Update date.py

--- date.py
+++ date.py
@@ -29,7 +29,7 @@ def movie_date(event,question,userid):
                     return 'ยังไม่ทราบวันฉายเลยครับ'
         if found == False:
             return 'ยังไม่ทราบวันฉายเลยครับ'
-    elif (movie_name == '') and (searchMovieNameInDic(question) == ''):
+    elif (movie_name == '') and (searchMovieNameInDic(question)[0] == ''):
         mov = findmovie(userid)
         movie_name = mov.lower().replace(' ','')
         URL = "http://mandm.plearnjai.com/API/id_nameMovie.php?key=mandm"
